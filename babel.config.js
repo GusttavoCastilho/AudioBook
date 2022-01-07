6cb233510acb681333101afa8f7b8121d30f7119
GusttavoCastilho <gustavocvalenciano@gmail.com>
2022-01-07 16:19:25 -0400

    feat: configure alias import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js", ".json"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@assets": "./src/assets",
+            "@hooks": "./src/hooks",
+            "@utils": "./src/utils",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
   };
 };
