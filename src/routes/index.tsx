056a744c26ff06ae767bd09f2cc47aa0c1aa7eba
GusttavoCastilho <gustavocvalenciano@gmail.com>
2022-02-05 14:14:50 -0400

    feat: add navigation bottom tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "@routes/app.routes";
+import { BottomTabs } from "@routes/bottom-tabs.routes";
+
+import { useAuth } from "@hooks/auth";
 
 export function Routes() {
+  const { user } = useAuth();
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user?.id ? <BottomTabs /> : <AppRoutes />}
     </NavigationContainer>
   );
 }
