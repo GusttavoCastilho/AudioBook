829b672211678cb4d45dac6b4e001b47401fb9cf
GusttavoCastilho <gustavocvalenciano@gmail.com>
2022-04-13 18:52:01 -0400

    fix: correctly placeholderTextColor from input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,9 @@
+import { TextInputProps } from 'react-native'
 import styled from "styled-components/native";
 
-export const Wrapper = styled.TextInput`
+export const Wrapper = styled.TextInput.attrs<TextInputProps>({
+  placeholderTextColor: "#B8B8C7",
+})`
   width: 100%;
   height: 53px;
   border-radius: 8px;
